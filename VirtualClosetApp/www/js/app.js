c96d98d930674eb1dafa52542a2408019a07324c
Zyaad <zkader@wisc.edu>
2017-03-09 17:11:26 -0600

    Test commit

--- VirtualClosetApp/www/js/app.js
+++ VirtualClosetApp/www/js/app.js
@@ -8,6 +8,8 @@ angular.module('starter', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
+      // changed code ZYAAD
+
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
