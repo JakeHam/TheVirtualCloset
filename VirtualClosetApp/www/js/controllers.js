aa256a3b5e95d55e0c2138a4efd4611f4a8279b5
Austin Mease <austinmease@Austins-MacBook-Pro.local>
2017-04-01 17:35:50 -0500

    loginLogout

--- VirtualClosetApp/www/js/controllers.js
+++ VirtualClosetApp/www/js/controllers.js
@@ -80,7 +80,32 @@ angular.module('starter.controllers', [])
   }
 
 })
+.controller('logoutCtrl', function($scope) {
+    alert("got to logout controller");
+ })
+ .controller('loginCtrl', function($scope, $ionicPopup, $state) {
+     $ionicModal.fromTemplateUrl('templates/menu/login.html',{
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+      $scope.modal.show();
 
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+     // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
+ })
 
  .controller('SearchCtrl', function($scope, $stateParams) {
    // getItems() {
