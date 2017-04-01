1b0a14568dd9cda66f7685411eb8b3290a268879
Austin Mease <austinmease@Austins-MacBook-Pro.local>
2017-04-01 17:54:54 -0500

    fixed linking issue

--- VirtualClosetApp/www/js/controllers.js
+++ VirtualClosetApp/www/js/controllers.js
@@ -10,61 +10,7 @@ angular.module('starter.controllers', [])
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/menu/login.html',{
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-
-  $ionicModal.fromTemplateUrl('templates/menu/logout.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
- // Triggered in the logout modal to close it
-  $scope.closeLogout = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the logout modal
-  $scope.logout = function() {
-    $scope.modal.show();
-
-  };
-
-   // Perform the logout action 
-  $scope.doLogout = function() {
-    console.log('Doing logout', $scope.logoutData);
-    // Simulate a logout delay when removing local memory 
-    $timeout(function() {
-      $scope.closeLogout();
-    }, 1000);
-  };
+  $scope.loginData = {}; 
 
  $scope.getItems = function(){
     // get from backend
